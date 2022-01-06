0d6eeed11eb38e9043b9988906e4f53f285ff33e
dsound-zz <demiansims@gmail.com>
2022-01-06 17:13:50 -0500

    wip

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
 import cors from "cors"
+import todosRoutes from "./routes/todos.js"
 
 const app = express()
 dotenv.config()
 app.use(express.json({ extended: true }))
 app.use(cors())
-const mongodb = MONGODB
+app.use("/todos", todosRoutes)
 app.get("/", (req, res) => {
  res.send("Welcome to server")
 })
 
 const PORT = process.env.PORT || 5000
+
+mongoose
+ .connect(process.env.MONGODB, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+ })
+ .then(() => console.log(`server is running on Port ${PORT}`))
+ .then(() => console.log(`server is running on port ${PORT}`))
+ .catch((err) => console.log(err))
